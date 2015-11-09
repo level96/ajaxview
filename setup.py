783a7e2b4bf8bf5e9eb81156218af22d2dcb84e7
Trung Phan <t.phan.an@level96.de>
2015-11-09 23:06:28 +0000

    Added: setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ajaxviews',
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Trung Phan',
+    author_email='t.phan.an@level96.de',
+    url='http://level96.de',
+    packages=['distutils', 'distutils.command'],
+)
